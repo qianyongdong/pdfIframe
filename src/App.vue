b99d36109f0b56d68b608b84b85f72bd05ea4873
yongdong.qian.wh@nikoyo.com.cn <yongdong.qian.wh@nikoyo.com.cn>
2023-03-06 16:03:52 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref } from "vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+const urls = [
+  {
+    url: "/1.pdf",
+    name: "1",
+  },
+  { url: "/2.pdf", name: "2" },
+];
+const pdfUrl = ref("");
+const dialogVisible = ref(false);
+const showIframe = (url) => {
+  console.log(url);
+  pdfUrl.value = url;
+  dialogVisible.value = true;
+};
+</script>
+
+<template>
+  <div>
+    <el-button v-for="url in urls" @click="showIframe(url.url)">{{
+      url.name
+    }}</el-button>
+    <el-dialog v-model="dialogVisible">
+      <iframe
+        :src="'/pdfjs-dist/web/viewer.html?file=' + pdfUrl"
+        frameborder="0"
+      ></iframe>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="dialogVisible = false">Cancel</el-button>
+        </span>
+      </template>
+    </el-dialog>
+  </div>
+</template>
+
+<style scoped></style>
